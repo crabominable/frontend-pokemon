ebc587ff933506c60037b323705ca00e3afc71f6
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2022-04-27 20:13:26 -0300

    feat and refactor: adding eslint and refactoring header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,33 +1,17 @@
-import {
-  ChevronLeftIcon,
-  ChevronRightIcon  } from '@chakra-ui/icons'
+import React from 'react'
 
 import './style.css'
 
-function Header() {
+function Header () {
   return (
     <div className="header-main">
       <div className="header-main-container">
-        <div className="header-title-container">
-          <p className="header-title">
-            Tech Pokedex
-          </p>
-        </div>
-        <button>
-          <ChevronLeftIcon w={6} h={6} color='#FFFFFF' />
-        </button>
-        <p className="header-letter-option-filter">
-          Letra
+        <p className="header-title">
+          Tech Pokedex
         </p>
-        <p className="header-number-option-filter">
-          Número
-        </p>
-        <button>
-          <ChevronRightIcon w={6} h={6} color='#FFFFFF' />
-        </button>
       </div>
     </div>
   )
 }
 
-export default Header;
+export default Header
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,15 @@
+import React from 'react'
+
 import { Flex } from '@chakra-ui/react'
 
-import Header from '../../components/Header';
+import Header from '../../components/Header'
 
-function Home() {
+const Home: React.FC = () => {
   return (
     <div>
       <Header />
-      <Flex align="center" justify="center">
-      </Flex>
     </div>
   )
 }
 
-export default Home;
+export default Home

3e89c931eb2b6e4ffbdf5a51919b731bfbcab02f
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2022-04-26 23:12:19 -0300

    refactor: refactoring header styles, putting in to style file

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,77 +1,32 @@
 import {
-  Text,
-  Flex } from "@chakra-ui/react";
-
-import {
   ChevronLeftIcon,
   ChevronRightIcon  } from '@chakra-ui/icons'
 
+import './style.css'
+
 function Header() {
   return (
-    <Flex
-      align="center"
-      borderRadius={6}
-      height={12}
-      justify="space-between"
-      margin={1}
-    >
-      <Flex
-        align='center'
-        backgroundColor='gray.100'
-        borderBottomLeftRadius={6}
-        borderTopLeftRadius={6}
-        height={12}
-        justify="center"
-        paddingLeft={6}
-        width="20%"
-      >
-        <Text
-          color='black.100'
-          fontFamily='Nunito'
-          fontSize={22}
-          fontWeight="bold"
-        >
-          Tech Pokedex
-        </Text>
-      </Flex>
-      <Flex
-        align='center'
-        backgroundColor='blackAlpha.800'
-        borderBottomRightRadius={6}
-        borderTopRightRadius={6}
-        height={12}
-        justify="center"
-        width="80%"
-      >
+    <div className="header-main">
+      <div className="header-main-container">
+        <div className="header-title-container">
+          <p className="header-title">
+            Tech Pokedex
+          </p>
+        </div>
         <button>
           <ChevronLeftIcon w={6} h={6} color='#FFFFFF' />
         </button>
-        <Text
-          color='#FFFFFF'
-          fontFamily='Nunito'
-          fontSize={20}
-          fontWeight="bold"
-          marginLeft={4}
-          marginRight={2}
-          opacity='40%'
-        >
+        <p className="header-letter-option-filter">
           Letra
-        </Text>
-        <Text
-          color='#FFFFFF'
-          fontFamily='Nunito'
-          fontSize={20}
-          fontWeight="bold"
-          marginLeft={2}
-          marginRight={4}
-        >
+        </p>
+        <p className="header-number-option-filter">
           Número
-        </Text>
+        </p>
         <button>
           <ChevronRightIcon w={6} h={6} color='#FFFFFF' />
         </button>
-      </Flex>
-    </Flex>
+      </div>
+    </div>
   )
 }
 
